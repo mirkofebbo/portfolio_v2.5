a6217d0a2036a1a3f87fd3f996ca7e3041f94573
mirkofebbo <mirkofebbo@gmail.com>
2025-02-14 19:55:39 +0000

    setup gh-pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,11 @@
-import React, { useEffect, useState } from "react";
-import { Typography, Box, Button } from "@mui/material";
+import { Typography, Box,  } from "@mui/material";
 
 const Home = () => {
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    document.title = `You clicked ${count} times`;
-  }, [count]);
 
   return (
     <Box>
-      <Typography variant="h1">Hello, World!</Typography>
-      <Typography variant="h2">You clicked {count} times</Typography>
-      <Button onClick={() => setCount(count + 1)}>Click me</Button>
+      <Typography variant="h1">Hi</Typography>
+      <Typography variant="h2">You clicked times</Typography>
     </Box>
   );
 }
